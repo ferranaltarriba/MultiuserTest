6740131d74e3e5a4a72f676db1e23e866b5d127c
ferranaltarriba <ferranaltarriba@gmail.com>
2021-01-22 11:55:41 +0100

    ksan

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
